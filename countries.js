3d9c72fc693d770ba5e70904752d829e33a3b0b7
shahed5358e <mohammadshahed5358@gmail.com>
2022-03-01 14:36:46 +0600

    meal db add

--- countries.js
+++ countries.js
@@ -10,16 +10,30 @@ const displayCountries = countries => {
     // }
     const countriesDiv = document.getElementById('countries');
     countries.forEach(country => {
-        console.log(country);
         const div = document.createElement('div');
         div.classList.add('country')
-        const h3 = document.createElement('h3');
-        h3.innerText = country;
-        div.appendChild(h3);
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        div.appendChild(p);
+        div.innerHTML = `
+        <h3> ${country.name.common} </h3>
+        <p> ${country.capital}</p>
+        <button onclick="loadCountryByName('${country.name.common}')">Details</button>
+         `;
         countriesDiv.appendChild(div);
-
     })
+}
+const loadCountryByName = name => {
+    const url = ` https://restcountries.com/v3.1/name/${name}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetail(data[0]));
+}
+
+const displayCountryDetail = country => {
+    console.log(country);
+    const countriesDiv = document.getElementById('country-details');
+    countriesDiv.innerHTML = `
+    <h4>Name: ${country.name.common}</h4>
+    <p>Population: ${country.population}</p>
+    <img width:"200px" src= "${country.flag}"/>
+    <p>Continents:${country.continents}</p>
+    `
 }
