15583363a35359289f7ec558f2061d8ce9254ee6
shahed5358e <mohammadshahed5358@gmail.com>
2022-03-01 14:48:07 +0600

    search text add

--- mealdb.js
+++ mealdb.js
@@ -20,11 +20,10 @@ const displaySearchResult = meals => {
         div.classList.add('col');
         div.innerHTML = `
             <div class="card">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.</p>
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${meal.strInstructions}</p>
                 </div>
             </div>
         `;
