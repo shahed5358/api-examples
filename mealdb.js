9fd7e184ed06383626f29a59a8ed3ec503013b1f
shahed5358e <mohammadshahed5358@gmail.com>
2022-03-01 15:04:54 +0600

    meald updated

--- mealdb.js
+++ mealdb.js
@@ -15,11 +15,11 @@ const searchFood = () => {
 const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
     meals.forEach(meal => {
-        console.log(meal);
+        // console.log(meal);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-            <div class="card">
+            <div onclick = "loadMealDetail(${meal.idMeal})" class="card">
                 <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${meal.strMeal}</h5>
@@ -29,4 +29,10 @@ const displaySearchResult = meals => {
         `;
         searchResult.appendChild(div);
     })
+}
+const loadMealDetail = mealId => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.meala))
 }
